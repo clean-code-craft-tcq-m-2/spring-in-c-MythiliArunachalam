29f1ddcd0508ee801689a31c5bb6c36a6b588017
MythiliArunachalam <115639100+MythiliArunachalam@users.noreply.github.com>
2022-10-22 20:08:32 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,52 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
-}
-
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+
+struct Stats 
+{  
+float average;
+float min;
+float max;
+};
+
+
+typedef void (*alerter_funcptr)();
+int main ()
+{
+struct Stats computedStats;
+/*    int setlength = sizeof(numberset) / sizeof(numberset[0]);
+    struct Stats computedStats = compute_statistics(numberset, setlength); */
+	computedStats = compute_statistics( const float numberset[], int setlength);
+
+	
+}
+
+struct Stats compute_statistics( const float numberset[], int setlength) {
+    
+	
+	int i;
+	float numtotal;
+	for (i=0, i<setlength,i++)
+	{
+	numtotal = numberset[i] + numtotal;
+	maxval = (numberset[i] >= maxval) ? numberset[i] : maxval;
+	minval = (numberset[i] <= minval) ? numberset[i] : minval];
+	}
+	
+	computedStats.average = numtotal/setlength;
+    computedStats.max = maxval;
+    computedStats.min = minval;
+	
+}
+
+void check_and_alert(const float maxThreshold, struct Stats computedStats);
+
+void check_and_alert(const float maxThreshold, struct Stats computedStats)
+{
+if (computedStats.max > maxThreshold)
+{
+	emailAlertCallCount = 1;
+	ledAlertCallCount = 1;
+}
+}
