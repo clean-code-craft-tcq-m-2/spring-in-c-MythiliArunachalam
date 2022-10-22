b85f2e522fa8215f69d0de3b3f1cba16ddbb1693
MythiliArunachalam <115639100+MythiliArunachalam@users.noreply.github.com>
2022-10-23 01:29:03 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -7,26 +7,6 @@ int check_and_alert(*alerters, const float maxThreshold, struct Stats computedSt
 
 struct Stats compute_statistics( const float numberset[], int setlength);
  
- void emailAlerter(const float maxThreshold,struct Stats computedStats)
- {
- if (computedStats.max > maxThreshold)
-	{
-	emailAlertCallCount = 1;
-	} 
- }
- void ledAlerter(const float maxThreshold,struct Stats computedStats)
- {
- if (computedStats.max > maxThreshold)
-	{
-	ledAlertCallCount = 1;
-	} 
- }
-
-typedef void alerter_funcptr(const float maxThreshold, struct Stats computedStats);
-int check_and_alert(alerter_funcptr *alerters ,const float maxThreshold, struct Stats computedStats)
-{ 
-return(alerters);
-} 
 int main ()
 {
 	const float maxThreshold = 10.2;
@@ -34,7 +14,24 @@ int main ()
 	int setlength = sizeof(numberset) / sizeof(numberset[0]);
 	struct Stats computedStats;
 	computedStats = compute_statistics(numberset, setlength);
-	check_and_alert(&emailAlerter,maxThreshold, computedStats);
-	check_and_alert(&ledAlerter,maxThreshold, computedStats);
  	return 0;
+}
+struct Stats compute_statistics( const float numberset[], int setlength) 
+{
+    	struct Stats statinfo;
+	int i;
+	float numtotal;
+	float maxval;
+	float minval;
+	for (i=0; i<setlength; i++)
+	{
+	numtotal = numberset[i] + numtotal;
+	maxval = (numberset[i] >= maxval) ? numberset[i] : maxval;
+	minval = (numberset[i] <= minval) ? numberset[i] : minval;
+	}
+   statinfo.average = numtotal/setlength;
+   statinfo.max = maxval;
+   statinfo.min = minval;
+   
+	return statinfo;
 }
