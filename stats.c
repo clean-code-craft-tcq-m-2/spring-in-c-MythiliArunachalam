def80d9476a536c406ca8574b6a2c646e5717683
MythiliArunachalam <115639100+MythiliArunachalam@users.noreply.github.com>
2022-10-22 20:52:19 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -11,12 +11,22 @@ float average;
 float min;
 float max;
 };
+struct Stats compute_statistics( const float numberset[], int setlength);
+
+int main ()
+{
+struct Stats computedStats;
+
+    computedStats = compute_statistics(numberset[], setlength); 
+}
+
 struct Stats compute_statistics( const float numberset[], int setlength) {
-    
-	struct Stats computedStats;
+    	struct Stats computedStats;
 	int i;
 	float numtotal;
-	for (i=0, i<setlength,i++;)
+	float maxval;
+	float minval;
+	for (i=0; i<setlength; i++;)
 	{
 	numtotal = numberset[i] + numtotal;
 	maxval = (numberset[i] >= maxval) ? numberset[i] : maxval;
@@ -28,14 +38,6 @@ struct Stats compute_statistics( const float numberset[], int setlength) {
     computedStats.min = minval;
 	return computedStats;
 	
-}
-
-int main ()
-{
-struct Stats computedStats;
-/*    int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength); */
-	computedStats = compute_statistics(numberset[],setlength);
 }
 
 void check_and_alert(const float maxThreshold, struct Stats computedStats)
