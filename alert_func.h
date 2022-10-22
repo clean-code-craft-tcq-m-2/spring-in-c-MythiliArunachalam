e685db00b78478b87bf3b4b9512caa58b25d6779
MythiliArunachalam <115639100+MythiliArunachalam@users.noreply.github.com>
2022-10-23 01:28:00 +0530

    Add files via upload

---
+++ alert_func.h
@@ -0,0 +1,8 @@
+
+struct Stats compute_statistics(const float* numberset, int setlength);
+
+typedef void alerter_funcptr(const float maxThreshold, struct computedStats);
+void check_and_alert(alerter_funcptr *alerters,float maxThreshold, struct Stats computedStats);
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
