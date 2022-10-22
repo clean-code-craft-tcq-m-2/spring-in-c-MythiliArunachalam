896ee690fb05a10605c6af386c1fab1ccd578513
MythiliArunachalam <115639100+MythiliArunachalam@users.noreply.github.com>
2022-10-23 00:54:52 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -3,48 +3,38 @@
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
-int check_and_alert(const float maxThreshold, *alerter_funcptr, struct computedStats)
-{
-	if (computedStats.max > maxThreshold)
-	{
-		return 1; 
-	}
-	else
-		return 0;
-}
+void check_and_alert(const float maxThreshold,*alerters, struct Stats computedStats);
 
 struct Stats compute_statistics( const float numberset[], int setlength);
  
-typedef void (*alerter_funcptr)();
- 
+ void emailAlerter(const float maxThreshold,struct computedStats)
+ {
+ if (computedStats.max > maxThreshold)
+	{
+	emailAlertCallCount = 1;
+	} 
+ }
+ void ledAlerter(const float maxThreshold,struct computedStats)
+ {
+ if (computedStats.max > maxThreshold)
+	{
+	ledAlertCallCount = 1;
+	} 
+ }
+
+typedef void alerter_funcptr(const float maxThreshold, struct computedStats);
+int check_and_alert(alerter_funcptr *alerters ,maxThreshold, computedStats);
+{ 
+return(alerters);
+} 
 int main ()
 {
 	const float maxThreshold = 10.2;
-	const float numberset[] = {1.5, 8.9, 3.2, 4.5};
+	float numberset[];
 	int setlength = sizeof(numberset) / sizeof(numberset[0]);
 	struct Stats computedStats;
 	computedStats = compute_statistics(numberset, setlength);
-	alerter_funcptr[] = {emailAlerter, ledAlerter};
-emailAlertCallCount = check_and_alert(maxThreshold, &emailAlerter, computedStats);
-ledAlertCallCount = check_and_alert(maxThreshold, &ledAlerter, computedStats);
+	check_and_alert(&emailAlerter,maxThreshold, computedStats);
+	check_and_alert(&ledAlerter,maxThreshold, computedStats);
  	return 0;
-}
-
-struct Stats compute_statistics( const float numberset[], int setlength) 
-{
-    	struct Stats statinfo;
-	int i;
-	float numtotal;
-	float maxval;
-	float minval;
-	for (i=0; i<setlength; i++)
-	{
-	numtotal = numberset[i] + numtotal;
-	maxval = (numberset[i] >= maxval) ? numberset[i] : maxval;
-	minval = (numberset[i] <= minval) ? numberset[i] : minval;
-	}
-   statinfo.average = numtotal/setlength;
-   statinfo.max = maxval;
-   statinfo.min = minval;
-	return statinfo;
 }
