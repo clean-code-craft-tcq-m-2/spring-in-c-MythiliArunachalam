db659ff0126203a5fececde20ed905b3162f8b2f
MythiliArunachalam <115639100+MythiliArunachalam@users.noreply.github.com>
2022-10-22 20:38:52 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -13,14 +13,14 @@ float max;
 };
 struct Stats compute_statistics( const float numberset[], int setlength) {
     
-	
+	struct Stats computedStats;
 	int i;
 	float numtotal;
-	for (i=0, i<setlength,i++)
+	for (i=0, i<setlength,i++;)
 	{
 	numtotal = numberset[i] + numtotal;
 	maxval = (numberset[i] >= maxval) ? numberset[i] : maxval;
-	minval = (numberset[i] <= minval) ? numberset[i] : minval];
+	minval = (numberset[i] <= minval) ? numberset[i] : minval;
 	}
 	
 	computedStats.average = numtotal/setlength;
@@ -35,7 +35,7 @@ int main ()
 struct Stats computedStats;
 /*    int setlength = sizeof(numberset) / sizeof(numberset[0]);
     struct Stats computedStats = compute_statistics(numberset, setlength); */
-	computedStats = compute_statistics( const float numberset[], int setlength);
+	computedStats = compute_statistics( float numberset[], int setlength);
 
 	
 }
