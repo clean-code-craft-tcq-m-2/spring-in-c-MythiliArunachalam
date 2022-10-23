245020bac26fd61f5bb4f10fb80dfeae6973a8d4
MythiliArunachalam <115639100+MythiliArunachalam@users.noreply.github.com>
2022-10-23 16:36:15 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,8 +7,8 @@ float max;
 };
 struct Stats compute_statistics(const float* numberset, int setlength);
 
-typedef void (alerter_funcptr *alerters, const float maxThreshold,struct Stats.computedStats );
-//void check_and_alert(alerter_funcptr *alerters,const float maxThreshold, struct Stats computedStats);
+typedef void (alerter_funcptr *(alerters), const float maxThreshold,struct computedStats );
+//void check_and_alert(alerter_funcptr *(alerters),const float maxThreshold, struct Stats computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
